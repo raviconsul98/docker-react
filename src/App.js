ad24c0462d8370a9ad71d05944068429f3fa4327
Ravi Consul <15UEC046@lnmiit.ac.in>
2020-05-15 00:00:40 +0530

    Updated name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There! Myself Ravi..
+          Hi There! Myself Ravi Consul..
         </p>
         <p>
           How are you!!
